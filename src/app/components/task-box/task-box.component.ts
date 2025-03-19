387a984313f2837c46da5144169b0683e1b0d4e9
Edward Kim <itgma1231@gmail.com>
2025-03-19 19:59:45 -0400

    ng add @fortawesome/angular-fontawesome; close icon added; swap to lts node version

--- src/app/components/task-box/task-box.component.ts
+++ src/app/components/task-box/task-box.component.ts
@@ -1,12 +1,16 @@
 import { Component, Input } from '@angular/core';
+import { NgStyle } from '@angular/common';
 import { Task } from '../../tasks';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-task-box',
-  imports: [],
+  imports: [FontAwesomeModule, NgStyle],
   templateUrl: './task-box.component.html',
   styleUrl: './task-box.component.css'
 })
 export class TaskBoxComponent {
   @Input() task!: Task;
+  faTimes = faTimes;
 }
