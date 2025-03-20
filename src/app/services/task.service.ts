afedfbdb58dc65b54ebd71911dca05516b745a9d
Edward Kim <itgma1231@gmail.com>
2025-03-20 11:53:19 -0400

    configurate HttpClient; call to api url at localhost:4300/tasks

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Task, TASKS } from '../tasks';
 import { Observable, of } from 'rxjs';
 
@@ -6,10 +7,13 @@ import { Observable, of } from 'rxjs';
   providedIn: 'root'
 })
 export class TaskService {
+  private apiUrl = 'http://localhost:4300/tasks';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   getTasks(): Observable<Task[]> {
-    return of(TASKS);
+    let data = this.http.get<Task[]>(this.apiUrl);
+    
+    return data;
   }
 }
