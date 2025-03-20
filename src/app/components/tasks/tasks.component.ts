9f8ec68ffd580bc165c27118a1647480d962b049
Edward Kim <itgma1231@gmail.com>
2025-03-20 10:46:38 -0400

    npm i json-server; package.json server script; db.json tasks added

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { CommonModule, NgFor } from '@angular/common';
-import { Observable, of } from 'rxjs';
+import { NgFor } from '@angular/common';
 import { TaskBoxComponent } from '../task-box/task-box.component';
 import { Task } from '../../tasks';
 import { TaskService } from '../../services/task.service';
@@ -8,18 +7,19 @@ import { TaskService } from '../../services/task.service';
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [CommonModule, NgFor, TaskBoxComponent],
+  imports: [NgFor, TaskBoxComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css',
 })
 export class TasksComponent implements OnInit {
-  tasks$: Observable<Task[]> = of([]);
+  tasks$: Task[] = [];
 
   constructor(private taskService: TaskService) {}
 
   ngOnInit(): void {
-    this.tasks$ = this.taskService.getTasks();
-    
-    this.tasks$.subscribe(data => console.log(data));
+    this.taskService.getTasks().subscribe((data) => {
+      this.tasks$ = data;
+      console.log(data)
+    })
   }
 }
