ebb03fcf58f35880574a9b6e5bbab3e79c916235
Edward Kim <itgma1231@gmail.com>
2025-03-24 17:45:15 -0400

    import FormsModule; two-way binding with [(ngModel)]

--- src/app/components/add-task/add-task.component.ts
+++ src/app/components/add-task/add-task.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-add-task',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './add-task.component.html',
   styleUrl: './add-task.component.css'
 })
 export class AddTaskComponent {
-
+  text?: string;
+  date?: string;
+  reminder: boolean = false;
 }
