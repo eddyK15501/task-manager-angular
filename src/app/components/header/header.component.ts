e6dad6c4c2b8ef993e1edddcd6bbfcfbe047d170
Edward Kim <itgma1231@gmail.com>
2025-03-27 19:29:47 -0400

    import rxjs Subscription; set boolean from UiService .onToggle(); btn color change

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,16 +1,26 @@
 import { Component } from '@angular/core';
 import { ButtonComponent } from '../button/button.component';
+import { UiService } from '../../services/ui.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   imports: [ButtonComponent],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
 })
 export class HeaderComponent {
   heading: string = 'Task Manager';
+  showAdd: boolean = false;
+  subscription: Subscription;
 
-  addTask() {
-    console.log('task added.')
+  constructor(private uiService: UiService) {
+    this.subscription = this.uiService
+      .onToggle()
+      .subscribe((value) => (this.showAdd = value));
+  }
+
+  toggleAddButton() {
+    this.uiService.toggleAddTask();
   }
 }
