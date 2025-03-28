111516390f70e51b9d44b65afcb554fcc8d346a3
Edward Kim <itgma1231@gmail.com>
2025-03-27 20:40:24 -0400

    toggle add-task form; import CommonModule for *ngIf to standalone AddTaskComponent

--- src/app/components/add-task/add-task.component.ts
+++ src/app/components/add-task/add-task.component.ts
@@ -1,10 +1,13 @@
 import { Component, Output, EventEmitter } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Task } from '../../tasks';
+import { UiService } from '../../services/ui.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-add-task',
-  imports: [FormsModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './add-task.component.html',
   styleUrl: './add-task.component.css',
 })
@@ -14,6 +17,14 @@ export class AddTaskComponent {
   text: string = '';
   day: string = '';
   reminder: boolean = false;
+  showAdd?: boolean;
+  subscription: Subscription;
+
+  constructor(private uiService: UiService) {
+    this.subscription = this.uiService
+      .onToggle()
+      .subscribe((value) => (this.showAdd = value));
+  }
 
   onFormSubmit() {
     if (this.text == '') {
